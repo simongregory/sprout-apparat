48457a0920ab666793fc42921033135ce04abe42
Simon Gregory <simon@helvector.org>
2012-03-25 18:39:21 +0100

    having a think about fetching the right scala version

--- lib/apparat.rb
+++ lib/apparat.rb
@@ -3,6 +3,7 @@ require 'sprout'
 require 'apparat/dump'
 require 'apparat/reducer'
 require 'apparat/version'
+require 'apparat/scala'
 
 Sprout::Specification.new do |s|
 
@@ -16,8 +17,20 @@ Sprout::Specification.new do |s|
     t.md5 = Apparat::MD5
     t.add_executable :dump, "apparat-#{Apparat::VERSION}/dump"
     t.add_executable :reducer, "apparat-#{Apparat::VERSION}/reducer"
-    #t.add_executable :apparat, "apparat"
-    #t.add_executable :coverage, "coverage"
-    #t.add_executable :tdsi, "tdsi"
   end
+
+  s.add_remote_file_target do |t|
+    t.platform     = :universal
+    t.archive_type = :zip
+    t.url = Scala::ZIP
+    t.md5 = Scala::MD5
+  end
+
+  #t.add_executable :asmifier, "apparat-#{Apparat::VERSION}/asmifier"
+  #t.add_executable :concrete, "apparat-#{Apparat::VERSION}/concrete"
+  #t.add_executable :coverage, "apparat-#{Apparat::VERSION}/coverage"
+  #t.add_executable :jitb, "apparat-#{Apparat::VERSION}/jitb"
+  #t.add_executable :stripper, "apparat-#{Apparat::VERSION}/stripper"
+  #t.add_executable :tdsi, "apparat-#{Apparat::VERSION}/tdsi"
+
 end
---
+++ lib/apparat/scala.rb
@@ -0,0 +1,7 @@
+module Scala
+  NAME    = 'scala'
+  VERSION = '1.8.2'
+  PLATFORM= 'Unix, Mac OS X, Cygwin'
+  MD5 = '8201780fcc796f3b68e401f8165c0002'
+  ZIP = "http://www.scala-lang.org/downloads/distrib/files/scala-2.8.2.final.tgz"
+end
